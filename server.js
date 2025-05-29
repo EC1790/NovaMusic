fe4376d1914b1e81330eae36a1a17abd5b290767
EC1790 <eshaanchowdhry@gmail.com>
2025-05-29 09:38:27 -0700

    yg

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
+const db = require("./db");
 
 const app = express();
 app.use(cors());
